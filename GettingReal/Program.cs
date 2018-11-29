05293391929210d8d57fe346b2ae2caf4d59fdb2
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2018-11-29 10:33:12 +0100

    Add project files.

---
+++ GettingReal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        private void Run()
+        {
+            SmartMenu menu = new SmartMenu();
+            menu.LoadMenu(@"..\..\MenuSpec.txt");
+            menu.Activate();
+        }
+    }
+}
---
+++ GettingReal/SmartMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal
+{
+    public class SmartMenu
+    {
+
+        public void LoadMenu(string path)
+        {
+
+        }
+
+        public void Activate()
+        {
+
+        }
+    }
+}
